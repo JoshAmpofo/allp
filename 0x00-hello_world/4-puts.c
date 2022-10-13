3a0847c7bf24966d36c63698c1754d61f61ca9a3
JoshAmpofo <joshampofo92@gmail.com>
2022-10-13 09:05:25 +0000

    Print out text using puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str1[80];
+
+	strcpy(str1, "Programming is like building a multilingual puzzle\n");
+	puts(str1);
+
+	return (0);
+}
