13756aa8056fc9dd75287abbf5af2a40acaac774
JoshAmpofo <joshampofo92@gmail.com>
2022-10-13 12:37:53 +0000

    Print string of text to stand error using printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char stderr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	printf("%s\n", stderr);
+	return (1);
+}
