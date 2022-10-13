e782912c942447d2b224a238d8557f79c2843ee3
JoshAmpofo <joshampofo92@gmail.com>
2022-10-13 11:20:46 +0000

    Print string of text using puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,10 +8,6 @@
  */
 int main(void)
 {
-	char str1[80];
-
-	strcpy(str1, "\"Programming is like building a multilingual puzzle");
-	puts(str1);
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
