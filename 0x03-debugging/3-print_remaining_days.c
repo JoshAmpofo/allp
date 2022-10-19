b622d7d69bdd000857ac6ed9f8608c6e9ff8d9e9
JoshAmpofo <joshampofo92@gmail.com>
2022-10-19 18:43:17 +0000

    Solve Date Error

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,14 +2,13 @@
 #include "main.h"
 
 /**
- * print_remaining_days -takes a date and prints how many days there are
- * left in the year, with leap years taken into consideration
- * @month: month in numerical format
- * @day: day of the month
+ * print_remaining_days - takes a date and prints how many days are
+ * left in the year, taking leap years into account
+ * @month: month in number format
+ * @day: day of month
  * @year: year
- *
  * Return: void
- */
+ * */
 
 void print_remaining_days(int month, int day, int year)
 {
@@ -23,14 +22,12 @@ void print_remaining_days(int month, int day, int year)
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
-
 	else
 	{
 		if (month == 2 && day == 60)
 		{
-		prinf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
-
 		else
 		{
 		printf("Day of the year: %d\n", day);
