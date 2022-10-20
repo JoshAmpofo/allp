e6b97b10bb3d66607ee407a70a47e30a46e07463
JoshAmpofo <joshampofo92@gmail.com>
2022-10-20 17:35:49 +0000

    Solve Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * main - print numbers from 1 to 100
+ * main - Entry point
  *
+ * Description: print numbers from 1 - 100
  * If number is a multiple of 3, return Fizz
  * If number is multiple of 5, return Buzz
  * If number is a multiple of both, return FizzBuzz
@@ -15,25 +16,15 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
-		{
 			printf("FizzBuzz");
-		}
 		else if (i % 3 == 0)
-		{
 			printf("Fizz");
-		}
 		else if (i % 5 == 0)
-		{
 			printf("Buzz ");
-		}
 		else
-		{
 			printf("%i", i);
-		}
 		if (i < 100)
-		{
 			printf(" ");
-		}
 	}
 	printf("\n");
 	return (0);
