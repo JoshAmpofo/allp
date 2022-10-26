c049b8f7cbccd52c15efc5243f2f5a2290154f35
JoshAmpofo <joshampofo92@gmail.com>
2022-10-26 23:00:10 +0000

    Change lowercase to UPPERCASE

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,12 +11,14 @@ char *string_toupper(char *s)
 {
 	int index;
 
+	index = 0;
+
 	/* iterate chars in string and check length */
 	while (*(s + index) != '\0')
 	{
 		if (*(s + index) >= 97  && *(s + index) <= 122) /* 97 = a, 122 = z */
 		{
-			*(s + index) = *(s + index) - 32; /* small case char to uppercase char */
+			*(s + index) -= 97 - 65;  /* small case char to uppercase char */
 		}
 		index++;
 	}
