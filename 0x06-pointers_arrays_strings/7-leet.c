5e1358a3c9bce9b38cc56eb0cece50431a1188e0
JoshAmpofo <joshampofo92@gmail.com>
2022-10-27 00:19:35 +0000

    Encode string to leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,31 +1,24 @@
 #include "main.h"
 
 /**
- * leet - encode a string into integer characters
+ * leet - encodes a string into 1337
+ * @s: string to encode
  *
- * @s: target string
- *
- * Only use one if and two loops
- * No ternary operator and switch usage
- *
- * Return: encoded string
+ * Return: encoded value of s
  */
 char *leet(char *s)
 {
 	int i, j;
-	char a[] = "aAeEoOtTlL"; /* predefine leet characters */
-	char b[] = "4433007711"; /* predefine leet corrs numbers */
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
 
-	/* iterate through strings */
 	for (i = 0; *(s + i); i++)
 	{
-		/* set new_string counter */
 		for (j = 0; j <= 9; j++)
 		{
-			/* convert given string to leet characters */
 			if (a[j] == s[i])
 				s[i] = b[j];
 		}
 	}
-	return (s); /* leet encoded characters */
+	return (s);
 }
