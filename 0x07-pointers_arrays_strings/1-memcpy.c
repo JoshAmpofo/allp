b27d1deded669c8014422ecf4bacaed17f08819d
JoshAmpofo <joshampofo92@gmail.com>
2022-10-31 14:21:11 +0000

    Copy memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,16 +10,19 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	/* set *dest and *src to new variables */
 	char *dest_str = (char *)dest;
 	char *src_str = (char *)src;
 
+	/* iterate through dest and str checking if len is not zero */
 	if ((dest_str != 0) && (src_str != 0))
 	{
-		while (n)
+		while (n) /* till size to print */
 		{
+			/* copy byte by byte */
 			*(dest_str++) = *(src_str++);
 			--n;
 		}
 	}
-	return (dest);
+	return (dest); /* print copied byte */
 }
