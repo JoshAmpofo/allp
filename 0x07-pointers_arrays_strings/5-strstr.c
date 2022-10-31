70896e214d1e930d8237c07853d72461a2d7abb5
JoshAmpofo <joshampofo92@gmail.com>
2022-10-31 17:06:47 +0000

    Locate substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strstr - searches a string for any of a set of bytes
+ * @needle: string containing characters to be matched
+ * @haystack: string to be matched
+ *
+ * Return: matched string or null if string non-existent
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	while (haystack[i])
+	{
+		while ((needle[j] && haystack[i] == needle[0]))
+		/* iterate through string to be matched */
+		{
+			if (haystack[i + j] == needle[j])
+			{
+				j++;
+			}
+			else
+				break;
+		}
+		if (needle[j])
+		{
+			i++; /* count byte for byte in s */
+			j = 0;
+		}
+		else
+			return (haystack + i);
+	}
+	return (0);
+}
