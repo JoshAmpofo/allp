fac7497c741e61d02356722c7308f04b033c0c83
JoshAmpofo <joshampofo92@gmail.com>
2022-10-31 15:24:23 +0000

    Header file

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,8 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
 int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
