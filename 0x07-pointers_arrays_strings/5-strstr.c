5236bd6ae14c48558ce56e4cd9830726f11e24b6
Joshua Ampofo Yentumi <78964254+JoshAmpofo@users.noreply.github.com>
2022-11-01 09:18:31 +0000

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,25 +11,25 @@ char *_strstr(char *haystack, char *needle)
 {
 	unsigned int i = 0, j = 0;
 
-	while (haystack[i])
+	while (haystack[i]) /* iterate through string to be matched byte by byte */
 	{
-		while ((needle[j] && haystack[i] == needle[0]))
+		while ((needle[j] && haystack[i] == needle[0])) /* check if each byte is same as index of string search */
 		/* iterate through string to be matched */
 		{
-			if (haystack[i + j] == needle[j])
+			if (haystack[i + j] == needle[j]) /* compare source string dest string */
 			{
-				j++;
+				j++; /* count byte of dest string */
 			}
 			else
 				break;
 		}
-		if (needle[j])
+		if (needle[j]) /* dest string byte */
 		{
-			i++; /* count byte for byte in s */
+			i++; /* count byte for byte in source */
 			j = 0;
 		}
 		else
-			return (haystack + i);
+			return (haystack + i); /* return byte of dest in source */
 	}
 	return (0);
 }
