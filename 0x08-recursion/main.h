bde24fa20b56f410ec2ac41956a7fb20836902b2
JoshAmpofo <joshampofo92@gmail.com>
2022-11-02 20:22:38 +0000

    Header file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,4 +11,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-#endif
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+#endif /* _MAIN_H_ */
