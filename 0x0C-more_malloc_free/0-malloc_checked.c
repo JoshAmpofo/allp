ff17e0504feb6a586157ac00517317f9e23db689
JoshAmpofo <joshampofo92@gmail.com>
2022-11-10 14:02:44 +0000

    Allocate memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * malloc_checked - allocate memory
+ * exit - exit status for normal process termination
+ * @b: value to store in memory
+ *
+ * Return: pointer to allocated memory, exit status 98 if fail
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *arr;
+	
+	/* allocate memory to arr */
+	arr = malloc(b);
+
+	/* if malloc fails */
+	if (arr == NULL)
+		exit(98); /* exit status */
+
+	return (arr);
+}
