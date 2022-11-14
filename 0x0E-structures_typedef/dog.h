97be6c4927d2676486c8a14897e1b8b76dd3a5e1
JoshAmpofo <joshampofo92@gmail.com>
2022-11-14 22:26:39 +0000

    Update header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,10 +16,14 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+/**
+ * dog_t - typedef for struct dog
+ */
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 int _strlen(char *str);
 char *_strcpy(char *dest, char *src);
 
 void free_dog(dog_t *d);
+
 #endif /* _DOG_H_ */
