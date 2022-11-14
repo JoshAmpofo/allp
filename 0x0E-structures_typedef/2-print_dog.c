e6445bea19515e11af9453b44f0fdad5f8e2f5a0
JoshAmpofo <joshampofo92@gmail.com>
2022-11-14 20:22:20 +0000

    Update solution

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,21 +9,26 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d->name != NULL)
+	if (d != NULL) /* as long as d is populated */
 	{
-		printf("Name: %s\n", d->name);
-	}
-	else
-	{
-		printf("Name: (nil)\n");
-	}
-	printf("Age: %.6f\n", d->age);
-	if (d->owner != NULL)
-	{
-		printf("Owner: %s\n", d->owner);
-	}
-	else
-	{
-		printf("Owner: (nil)\n");
-	}
+		if (d->name != NULL) /* print name */
+		{
+			printf("Name: %s\n", d->name);
+		}
+		else
+		{
+			printf("Name: (nil)\n"); /* print error msg */
+		}
+
+		/* Age cannot be null so no else statement */
+		printf("Age: %.6f\n", d->age);
+
+		if (d->owner != NULL)
+		{
+			printf("Owner: %s\n", d->owner); /* print owner name */
+		}
+		else
+		{
+			printf("Owner: (nil)\n");
+		}
 }
