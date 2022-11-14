ff1f934dc5699397c2ae670aefd41fc1e4fd148f
JoshAmpofo <joshampofo92@gmail.com>
2022-11-14 18:53:26 +0000

    Define struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
-struct dog 
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - define dog type
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif /* _DOG_H_ */
