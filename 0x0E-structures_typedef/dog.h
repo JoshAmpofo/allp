b0053c75b6702dee2e7f78554147ed0f095d22f6
JoshAmpofo <joshampofo92@gmail.com>
2022-11-14 20:16:26 +0000

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+
 /**
  * struct dog - define dog type
  * @name: dog name
@@ -13,4 +14,9 @@ struct dog
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif /* _DOG_H_ */
