050517cec3c5e375745de761fc9bd8fc39419e0e
Joshua Ampofo Yentumi <78964254+JoshAmpofo@users.noreply.github.com>
2022-11-15 21:40:41 +0000

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _3_FUNCTION_LIKE_MACRO_
-#define _3_FUNCTION_LIKE_MACRO_
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
 
 #define ABS(x) ((x) < 0 ? (-(x)) : (x))
 
-#endif /* _3_FUNCTION_LIKE_MACRO */
+#endif /* _3_FUNCTION_LIKE_MACRO_H_ */
