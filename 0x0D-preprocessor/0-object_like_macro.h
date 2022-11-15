dae6a3b3d949ea159cccfa9a1b26a542a3f25963
Joshua Ampofo Yentumi <78964254+JoshAmpofo@users.noreply.github.com>
2022-11-15 21:17:13 +0000

    Update 0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _0-OBJECT_LIKE_MACRO_
-#define _0-OBJECT_LIKE_MACRO__
+#ifndef _0_OBJECT_LIKE_MACRO_
+#define _0_OBJECT_LIKE_MACRO__
 
 #define SIZE 1024
 
-#endif /* _0-OBJECT_LIKE_MACRO_ */
+#endif /* _0_OBJECT_LIKE_MACRO_ */
