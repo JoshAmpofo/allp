f3040da72589125a0bacb9db32b4f2e814612296
JoshAmpofo <joshampofo92@gmail.com>
2022-11-24 14:53:25 +0000

    Print elements in lin-list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - print all the elements of a list_t list
+ * @h: pointer to address of node
+ *
+ * Return: number of nodes and content
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
