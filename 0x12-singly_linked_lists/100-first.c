3340bf16f855feb4a77a11f73454dfebbf405b69
JoshAmpofo <joshampofo92@gmail.com>
2022-11-24 17:04:53 +0000

    Print output before main runs

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) hare(void);
+
+/**
+ * hare - prints a string before the main function 
+ * is executed...this function is amazing
+ * i never knew something like this existed
+ * You learn something new everyday!
+ *
+ * Return: void
+ */
+void hare(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
