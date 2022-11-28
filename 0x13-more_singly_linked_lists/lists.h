35709cd1458c5eb6b9b5d5776f62a1087c000179
JoshAmpofo <joshampofo92@gmail.com>
2022-11-28 19:03:54 +0000

    Fix betty error

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -37,7 +37,8 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-const listint_t **memalloc(const listint_t **list, size_t size, const listint_t *new);
+const listint_t **memalloc(const listint_t **list, size_t size,
+		const listint_t *new);
 listint_t **alloc(listint_t **list, size_t size, listint_t *new);
 
 #endif /* _LISTS_H_ */
