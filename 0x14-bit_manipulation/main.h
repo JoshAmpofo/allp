60676046603017da2808a0178778b05cde3ab359
JoshAmpofo <joshampofo92@gmail.com>
2022-12-01 18:25:48 +0000

    Add function prototype

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -10,5 +10,6 @@ int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 int _putchar(char);
+int get_endianness(void);
 
 #endif /* _MAIN_H_ */
