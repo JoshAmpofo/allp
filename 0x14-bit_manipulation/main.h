36cc476a15c8a04cdd6171cd37747df8a2dd0d01
JoshAmpofo <joshampofo92@gmail.com>
2022-12-01 17:06:17 +0000

    Add _putchar function

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,5 +9,6 @@ int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
+int _putchar(char);
 
 #endif /* _MAIN_H_ */
