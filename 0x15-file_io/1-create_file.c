1fde398df9d3fa7a76ea7b3037d980a8f959d2b8
JoshAmpofo <joshampofo92@gmail.com>
2022-12-05 18:54:48 +0000

    Fix error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	/* create variables to open, write and find length of str */
-	int opn, wr, str_len;
+	int opn, wr, str_len = 0;
 
 	/* set fail condition for filename */
 	if (filename == NULL)
