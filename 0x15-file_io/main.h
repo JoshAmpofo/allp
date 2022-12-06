1e125fcdbc8d72e7dfab33ccd291c0d9a8994bf5
JoshAmpofo <joshampofo92@gmail.com>
2022-12-06 03:14:35 +0000

    Update function prototypes

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,10 +7,25 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <elf.h>
 
 int _putchar(char);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+char *create_buffer(char *filename);
+void close_file(int fd);
+
+/* elf file prototypes */
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 #endif /* _MAIN_H_ */
