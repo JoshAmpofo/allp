b8db091e30f847e5a5c9d0f8edace940238f28b7
JoshAmpofo <joshampofo92@gmail.com>
2023-01-12 09:37:41 +0000

    Print all elements in a DLL

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print all the elements of a doubly linked list
+ * @h: pointer to head node
+ *
+ * Return: number of nodes in list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+	const dlistint_t *current = h;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		count++;
+	}
+	return (count);
+}
