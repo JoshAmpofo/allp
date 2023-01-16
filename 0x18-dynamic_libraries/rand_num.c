5f03961f5f1dab72bd723bebe7ca5978331f0188
JoshAmpofo <joshampofo92@gmail.com>
2023-01-16 15:24:47 +0000

    Fix betty error

--- 0x18-dynamic_libraries/rand_num.c
+++ 0x18-dynamic_libraries/rand_num.c
@@ -1,22 +1,27 @@
 #include <unistd.h>
 #include <string.h>
 
-int randm()
+/**
+ * randm - generate random numbers
+ *
+ * Return: winning numbers
+ */
+int randm(void)
 {
 	static int rand_n = -1;
 
 	rand_n++;
 	if (rand_n == 0)
-		return 8;
+		return (8);
 	if (rand_n == 1)
-		return 8;
+		return (8);
 	if (rand_n == 2)
-		return 7;
+		return (7);
 	if (rand_n == 3)
-		return 9;
+		return (9);
 	if (rand_n == 4)
-		return 23;
+		return (23);
 	if (rand_n == 5)
-		return 74;
-	return rand_n * rand_n % 30000;
+		return (74);
+	return (rand_n * rand_n % 30000);
 }
