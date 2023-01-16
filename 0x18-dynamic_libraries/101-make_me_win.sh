a0ca34363f2a9927a1400d11801cb5c408f4168f
JoshAmpofo <joshampofo92@gmail.com>
2023-01-16 14:30:57 +0000

    Fix error

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/JoshAmpofo/alx-low_level_programming/blob/main/0x18-dynamic_libraries/rand_num.so
-export LD_PRELOAD=/tmp/rand_num.so
+wget -P /tmp
+export LD_PRELOAD=/tmp/
