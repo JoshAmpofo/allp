b81e8bca79bfece2c479786ed01fbfb414f4c557
JoshAmpofo <joshampofo92@gmail.com>
2023-02-03 08:05:53 +0000

    Add more function prototypes

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -41,7 +41,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
-
+hash_node_t *make_node(const char *key, const char *value);
 /**
  * hash_djb2 - implementation of the djb2 algorithm
  * @str: string to generate hash value for
