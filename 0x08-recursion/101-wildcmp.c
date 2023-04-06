d0f869a160b8f7245195e7c8bd3f7b89b5b3bb02
JoshAmpofo <joshampofo92@gmail.com>
2023-04-06 12:52:07 +0000

    Fix Betty error

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -18,7 +18,8 @@ int chk_str(char *s1, char *s2, int a, int sc)
 	if (s1[a] == '\0' && s2[sc] == '*')
 		return (chk_str(s1, s2, a, sc + 1));
 	if (s2[sc] == '*')
-		return (chk_str(s1, s2, a + 1, sc) || chk_str(s1, s2, a, sc + 1));
+		return (chk_str(s1, s2, a + 1, sc) || 
+				chk_str(s1, s2, a, sc + 1));
 	return (0);
 }
 
